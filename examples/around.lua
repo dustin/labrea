4306a8dffbc1c6176e907cbc4eadfc00a4a25b73
Dustin Sallings <dustin@spy.net>
2010-11-18 23:44:04 -0800

    Use stderr in the around example (and added lseek).

--- examples/around.lua
+++ examples/around.lua
@@ -1,13 +1,20 @@
 truncateto = 8
 
 function around_read(fname, d, buf, size)
-   io.write(string.format("Doing an around read with %s(%d, %x, %d).\n", fname, d, buf, size))
+   io.stderr:write(string.format("Doing an around read with %s(%d, %x, %d).\n", fname, d, buf, size))
    if size > truncateto
    then
-      io.write(string.format("Truncating read of %d to %d\n", size, truncateto))
+      io.stderr:write(string.format("Truncating read of %d to %d\n", size, truncateto))
       size = truncateto
    end
    rv = invoke(fname, d, buf, size)
-   io.write(string.format("...and returning %d bytes read\n", rv))
+   io.stderr:write(string.format("...and returning %d bytes read\n", rv))
+   return rv
+end
+
+function around_lseek(f, fd, offset, whence)
+   io.stderr:write(string.format("Doing a seek on %d of %d from %d\n", fd, offset, whence))
+   rv = invoke(f, fd, offset, whence)
+   io.stderr:write(string.format("...returned %d\n", rv))
    return rv
 end
