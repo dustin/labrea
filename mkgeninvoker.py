56383753195a9d11246cea28e4ecb47085570f13
Dustin Sallings <dustin@spy.net>
2010-11-19 09:52:13 -0800

    Support older version of python in code generation.

--- mkgeninvoker.py
+++ mkgeninvoker.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-import itertools
 
 def type(s):
     return {4: 'uint32_t', 8: 'uint64_t'}[int(s)]
@@ -16,8 +15,22 @@ def encode(r, argseq):
         i = (i << 1) | b
     return (i << 1) | int(int(r) == 8)
 
+def decode(offset, length):
+    def s(n):
+        return int('48'[n])
+
+    r = s(offset & 1)
+    offset = offset >> 1
+    seq = list(reversed([s((offset >> i) & 1) for i in range(length)]))
+    return r, seq
+
 def genfname(argc, r, argwidth):
     return 'invoke_%d_%s_%s' % (argc, r, ''.join(argwidth))
+
+def mk_argseq(l):
+    def strdecode(a, b):
+        return ''.join([str(x) for x in decode(a, b)[1]])
+    return sorted(set(strdecode(x, l) for x in range(int(2 ** (l+1)))))
 
 def gen(out, argc, r, argwidth):
     argtypes = '%s' % ', '.join(type(a) for a in argwidth)
@@ -47,7 +60,7 @@ namespace labrea {
 """)
 
     for i in range(1, MAXARGS+1):
-        for argseq in sorted(set(itertools.permutations("48" * i, i))):
+        for argseq in mk_argseq(i):
             for r in '48':
                 gen(out, i, r, argseq)
 
@@ -56,8 +69,8 @@ namespace labrea {
         callargs = ', '.join('a%d' % x for x in range(i))
         out.write("static lua_Integer abstractInvoke(struct ftype *fun, %s) {\n" % fargs)
         out.write("    assert(fun->num_args == %d);\n" % i)
-        allfuns = dict((encode(4, seq), seq) for seq in set(itertools.permutations("48" * i, i)))
-        allfuns.update(dict((encode(8, seq), seq) for seq in set(itertools.permutations("48" * i, i))))
+        allfuns = dict((encode(4, seq), seq) for seq in mk_argseq(i))
+        allfuns.update(dict((encode(8, seq), seq) for seq in mk_argseq(i)))
         out.write("    static lua_Integer (*allfuns[])(const void*, %s) = {\n" % fargs)
         for o in range(len(allfuns)):
             if o in allfuns:
